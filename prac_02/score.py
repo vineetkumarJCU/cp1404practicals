f939ef1b09ab82dac4a346ca848ae1c880500a61
vineetkrr <vineet.kumar@my.jcu.edu.au>
2025-09-28 02:44:19 +1000

    Refactor score file to follow SRP

--- prac_02/score.py
+++ prac_02/score.py
@@ -3,12 +3,25 @@ CP1404/CP5632 - Practical
 Program to determine score status
 """
 
-score = float(input("Enter score: "))
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+import random
+
+def main():
+    score = float(input("Enter score: "))
+    category = (get_result(score))
+    print(f"A score of {score} is {category}")
+
+    # Generate random score
+    random_score = random.randint(0, 100)
+    print(f"Random score: {random_score})
+
+def get_result(score):
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+main()

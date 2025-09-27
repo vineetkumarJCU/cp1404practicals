dcf4ed8823831fdcc4befc4a08c974b2ba9bb414
vineetkrr <vineet.kumar@my.jcu.edu.au>
2025-09-27 22:20:00 +1000

    Add password check program

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,8 +1,10 @@
 MINIMUM_LENGTH = 8
 
-password = input("Enter Password:")
-while len(password) < 8:
-    print(f"Password should be {MINIMUM_LENGTH} characters long")
+def main():
     password = input("Enter Password:")
-if len(password) > 8:
+    while len(password) < 8:
+        print(f"Password should be {MINIMUM_LENGTH} characters long.")
+        password = input("Enter Password:")
     print("*"*len(password))
+
+main()

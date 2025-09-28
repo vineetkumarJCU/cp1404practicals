ed6668ef4b42390e2cd22c963050c43a2edb259a
vineetkrr <vineet.kumar@my.jcu.edu.au>
2025-09-28 17:32:43 +1000

    Added inline comments and docstrings

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,15 +1,18 @@
-MINIMUM_LENGTH = 8
+MINIMUM_LENGTH = 8 # Minimum length of password
 
 def main():
+    """Main function to get and print password"""
     password = get_password()
     print_asterisks(password)
 
 
 def print_asterisks(password):
+    """Print the asterisks based on the password length"""
     print("*" * len(password))
 
 
 def get_password():
+    """Ask the user for password until they enter valid password with required length"""
     password = input("Enter Password:")
     while len(password) < 8:
         print(f"Password should be {MINIMUM_LENGTH} characters long.")
--- prac_02/score.py
+++ prac_02/score.py
@@ -6,6 +6,7 @@ Program to determine score status
 import random
 
 def main():
+    """Ask user for a score and then print result and a random score."""
     score = float(input("Enter score: "))
     category = (get_result(score))
     print(f"A score of {score} is {category}")
@@ -15,6 +16,7 @@ def main():
     print(f"Random score: {random_score})
 
 def get_result(score):
+    """Determine the score based on the result"""
     if score < 0 or score > 100:
         return "Invalid score"
     elif score >= 90:

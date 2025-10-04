7d822971e7c058bef7b1bd513e46bb9bc5ccf331
vineetkrr <vineet.kumar@my.jcu.edu.au>
2025-10-05 01:28:58 +1000

    Add while loop to avoid ZeroDivisionError

--- prac_03/exceptions_demo.py
+++ prac_03/exceptions_demo.py
@@ -13,10 +13,11 @@ for example: hello, $, {, nineteen
 try:
     numerator = int(input("Enter the numerator: "))
     denominator = int(input("Enter the denominator: "))
+    while denominator == 0:
+        print("Denominator can't be 0. Please try again!")
+        denominator = int(input("Enter the denominator: "))
     fraction = numerator / denominator
     print(fraction)
 except ValueError:
     print("Numerator and denominator must be valid numbers!")
-except ZeroDivisionError:
-    print("Cannot divide by zero!")
 print("Finished.")

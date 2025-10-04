7335f237c08e40e7b7c6aa6f2321822270d24170
vineetkrr <vineet.kumar@my.jcu.edu.au>
2025-10-04 23:44:21 +1000

    Add randoms.py with answers from Python console using randint, randrange, and uniform

---
+++ prac_03/randoms.py
@@ -0,0 +1,21 @@
+# What did you see on line 1?
+# 18
+# What was the smallest number you could have seen, what was the largest?
+# The smallest number I saw was 5 and the largest I saw was 20.
+
+# What did you see on line 2?
+# 9
+# What was the smallest number you could have seen, what was the largest?
+# Smallest number 3 and largest number 9.
+# Could line 2 have produced a 4?
+# No, because function is set to only produce odd numbers in steps of 2 starting from number 3 upto 10.
+
+# What did you see on line 3?
+# 3.272595125323031
+# What was the smallest number you could have seen, what was the largest?
+# Smallest 2.5 and largest 5.5.
+
+# Write code, not a comment, to produce a random number between 1 and 100 inclusive.
+import random
+
+print(random.randint(1,100))

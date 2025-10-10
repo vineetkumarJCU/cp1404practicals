1e51125ab7d2867dbde8ec5a19507100d90fd96d
vineetkrr <vineet.kumar@my.jcu.edu.au>
2025-10-10 22:26:40 +1000

    "Modify load_data to return list of lists

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -13,17 +13,14 @@ def main():
 
 def load_data(filename=FILENAME):
     """Read data from file formatted like: subject,lecturer,number of students."""
+    data = []
     input_file = open(filename)
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
         line = line.strip()  # Remove the \n
         parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
+        data.append(parts)
     input_file.close()
-
+    return data
 
 main()

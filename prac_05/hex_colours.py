d4126d0ed582066cd8b7f7434c3abe757563caaa
vineetkrr <vineet.kumar@my.jcu.edu.au>
2025-10-16 17:56:03 +1000

    Add base colours to hex dictionary

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,17 @@
+"""
+CP1404/CP5632 Practical
+Hex Colours Program
+"""
+
+COLOUR_TO_HEX = {
+    "AliceBlue": "#f0f8ff",
+    "Amaranth": "#e52b50",
+    "Amber": "#ffbf00",
+    "Amethyst": "#9966cc",
+    "AntiqueWhite": "#faebd7",
+    "Apricot": "#fbceb1",
+    "Beaver": "#9f8170",
+    "Black": "#000000",
+    "Beige": "#f5f5dc",
+    "Burgundy": "#800020"
+}

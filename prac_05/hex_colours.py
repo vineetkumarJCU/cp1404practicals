1d2dcffdc1b5721e5647cd06bd77552bcff184fe
vineetkrr <vineet.kumar@my.jcu.edu.au>
2025-10-16 18:54:35 +1000

    Finish hex_colourswith error handling

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -15,3 +15,17 @@ COLOUR_TO_HEX = {
     "Beige": "#f5f5dc",
     "Burgundy": "#800020"
 }
+
+
+def main():
+    """Hexadecimal colour codes by name."""
+    colour_name = input("Enter colour name: ").title()
+    while colour_name != "":
+        try:
+            print(f"{colour_name} is {COLOUR_TO_HEX[colour_name]}")
+        except KeyError:
+            print("Invalid colour name")
+        colour_name = input("Enter colour name: ").title()
+
+
+main()

7f9b10b93728bbcb590f5f50c9d85e29049018df
vineetkrr <vineet.kumar@my.jcu.edu.au>
2025-10-19 12:17:47 +1100

    Complete and Refactor emails program into functions

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -1,11 +1,17 @@
 """
 Emails File
 Estimate: 15 minutes
-Actual:
+Actual: 26 minutes
 """
 
 def main():
     """Store users emails and names in a dictionary."""
+    email_to_name = get_emails_and_names()
+    display_emails(email_to_name)
+
+
+def get_emails_and_names():
+    """Prompt user for emails and return a dictionary."""
     email_to_name = {}
     email = input("Email: ").strip()
     while email != "":
@@ -15,7 +21,11 @@ def main():
             name = input("Name: ").title()
         email_to_name[email] = name
         email = input("Email: ").strip()
+    return email_to_name
 
+
+def display_emails(email_to_name):
+    """Print all stored names and emails."""
     for email, name in email_to_name.items():
         print(f"{name} ({email})")
 

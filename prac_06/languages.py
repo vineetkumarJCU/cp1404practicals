d46b05abebb5817272a6dff615ab8f53d7c81cba
vineetkrr <vineet.kumar@my.jcu.edu.au>
2025-11-02 09:50:34 +1100

    Finish languages.py client to display and filter ProgrammingLanguage objects

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -3,3 +3,22 @@ CP1404: Practical 06
 Estimate Time: 25 minutes
 Actual Time:
 """
+
+from programming_language import ProgrammingLanguage
+
+
+def main():
+    """Create and display ProgrammingLanguage objects."""
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+    print(python)
+
+    languages = [python, ruby, visual_basic]
+    print("\nThe dynamically typed languages are:")
+    for language in languages:
+        if language.is_dynamic():
+            print(language.name)
+
+
+main()

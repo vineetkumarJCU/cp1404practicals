2b5c05e24a907d4eecbe59222d10e7817a5be40b
vineetkrr <vineet.kumar@my.jcu.edu.au>
2025-11-02 09:30:42 +1100

    Define __init__ and is_dynamic

---
+++ prac_06/programming_language.py
@@ -0,0 +1,13 @@
+class ProgrammingLanguage:
+    """Represent a programming language."""
+
+    def __init__(self, name, typing, reflection, year):
+        """Initialise a ProgrammingLanguage instance."""
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    def is_dynamic(self):
+        """Return True if the language is dynamically typed."""
+        return self.typing.lower() == "dynamic"

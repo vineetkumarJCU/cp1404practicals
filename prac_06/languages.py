3e94191a46bed8111fa7139b6fca23aac5b43450
vineetkrr <vineet.kumar@my.jcu.edu.au>
2025-11-02 10:08:20 +1100

    Remove temporary print(python) test

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -12,7 +12,6 @@ def main():
     python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
     ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
     visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
-    print(python)
 
     languages = [python, ruby, visual_basic]
     print("\nThe dynamically typed languages are:")

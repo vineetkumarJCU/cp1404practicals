2bd0c286ccd73450669b59bd56d9c6bd516a7d34
vineetkrr <vineet.kumar@my.jcu.edu.au>
2025-11-02 22:55:12 +1000

    Finish guitars.py exercise

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -1,5 +1,32 @@
 """
 CP1404/CP5632 Practical 06
 Estimate: 25 minutes
-Actual:
+Actual: 49 minutes
 """
+from guitar import Guitar
+
+
+def main():
+    """Store user’s guitars in a list and display their details."""
+    print("My guitars!")
+    guitars = []
+
+    name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: $"))
+        guitar = Guitar(name, year, cost)
+        guitars.append(guitar)
+        print(f"{guitar} added.\n")
+        name = input("Name: ")
+
+    if guitars:
+        print("These are my guitars:")
+        for i, guitar in enumerate(guitars, 1):
+            vintage_string = " (vintage)" if guitar.is_vintage() else ""
+            print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), "
+                  f"worth ${guitar.cost:10,.2f}{vintage_string}")
+    else:
+        print("No guitars :( Quick, go and buy one!")
+
+main()

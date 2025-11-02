21d282d73e408acfe0df8b77dfabf3c45c60b388
vineetkrr <vineet.kumar@my.jcu.edu.au>
2025-11-02 21:29:10 +1000

    Finish guitar_test to test get_age and is_vintage methods

--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -1,5 +1,22 @@
 """
 CP1404/CP5632 Practical 06
 Estimate: 20 minutes
-Actual:
+Actual: 28 minutes
 """
+
+from guitar import Guitar
+
+
+def main():
+    """Test get_age and is_vintage methods of Guitar class."""
+    gibson = Guitar("Gibson L-5 CES", 1922, 16035.40)
+    another = Guitar("Another Guitar", 2013, 500)
+
+    print(f"{gibson.name} get_age() - Expected 103. Got {gibson.get_age()}")
+    print(f"{another.name} get_age() - Expected 12. Got {another.get_age()}")
+
+    print(f"{gibson.name} is_vintage() - Expected True. Got {gibson.is_vintage()}")
+    print(f"{another.name} is_vintage() - Expected False. Got {another.is_vintage()}")
+
+
+main()

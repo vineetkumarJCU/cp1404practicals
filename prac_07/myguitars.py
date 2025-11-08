766b72c87a6f72e45702b6461c0f62b32782ac6f
vineetkrr <vineet.kumar@my.jcu.edu.au>
2025-11-08 23:20:50 +1000

    Finish by implementing separate functions

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -2,7 +2,7 @@
 CP1404/CP5632 Practical
 More Guitars
 Estimate: 30 minutes
-Actual:
+Actual: 78 minutes
 """
 
 from guitar import Guitar
@@ -16,6 +16,19 @@ def main():
     for guitar in guitars:
         print(guitar)
 
+    # Sort by year
+    guitars.sort()
+    print("\nSorted guitars (oldest to newest):")
+    for guitar in guitars:
+        print(guitar)
+
+    # Add new guitars from user
+    add_new_guitars(guitars)
+
+    # Save guitars to file
+    save_guitars("guitars.csv", guitars)
+
+
 def load_guitars(filename):
     """Load guitars from CSV into a list of Guitar objects."""
     guitars = []
@@ -27,6 +40,26 @@ def load_guitars(filename):
             cost = float(parts[2])
             guitars.append(Guitar(name, year, cost))
     return guitars
+
+
+def add_new_guitars(guitars):
+    """Ask user to add new guitars."""
+    print("\nAdd your new guitars:")
+    name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: $"))
+        new_guitar = Guitar(name, year, cost)
+        guitars.append(new_guitar)
+        print(f"{new_guitar} added.\n")
+        name = input("Name: ")
+
+
+def save_guitars(filename, guitars):
+    """Save guitars to a CSV file."""
+    with open(filename, "w", encoding="utf-8-sig") as out_file:
+        for guitar in guitars:
+            print(f"{guitar.name},{guitar.year},{guitar.cost}", file=out_file)
 
 
 main()

90e96eda59ea07a3867d91b1dbdfbf5cafdab2ac
vineetkrr <vineet.kumar@my.jcu.edu.au>
2025-11-08 20:12:39 +1000

    Update language_file_reader to handle Pointer Arithmetic

--- prac_07/language_file_reader.py
+++ prac_07/language_file_reader.py
@@ -13,24 +13,33 @@ from programming_language import ProgrammingLanguage
 def main():
     """Read file of programming language details, save as objects, display."""
     languages = []
+
     # Open the file for reading
     in_file = open('languages.csv', 'r')
+
     # File format is like: Language,Typing,Reflection,Year
     # 'Consume' the first line (header) - we don't need its contents
     in_file.readline()
+
     # All other lines are language data
     for line in in_file:
         # print(repr(line))  # debugging
+
         # Strip newline from end and split it into parts (CSV)
         parts = line.strip().split(',')
         # print(parts)  # debugging
-        # Reflection is stored as a string (Yes/No) and we want a Boolean
+
+        # Convert string values to appropriate types
+        name = parts[0]
+        typing = parts[1]
         reflection = parts[2] == "Yes"
-        # Construct a ProgrammingLanguage object using the elements
-        # year should be an int
-        language = ProgrammingLanguage(parts[0], parts[1], reflection, int(parts[3]))
-        # Add the language we've just constructed to the list
+        pointer_arithmetic = parts[3] == "Yes"
+        year = int(parts[4])
+
+        # Create a ProgrammingLanguage object
+        language = ProgrammingLanguage(name, typing, reflection, pointer_arithmetic, year)
         languages.append(language)
+
     # Close the file as soon as we've finished reading it
     in_file.close()
 

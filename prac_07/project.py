91a8f805e1c764bafc0b015bef96eb84ce8bb200
vineetkrr <vineet.kumar@my.jcu.edu.au>
2025-11-09 19:08:17 +1000

    Implement __str__ method

--- prac_07/project.py
+++ prac_07/project.py
@@ -13,3 +13,10 @@ class Project:
         self.priority = priority
         self.cost_estimate = cost_estimate
         self.completion_percentage = completion_percentage
+
+
+    def __str__(self):
+        return (f"{self.name}, start: {self.start_date.strftime('%d/%m/%Y')}, "
+                f"priority {self.priority}, estimate: ${self.cost_estimate:,.2f}, "
+                f"completion: {self.completion_percentage}%")
+

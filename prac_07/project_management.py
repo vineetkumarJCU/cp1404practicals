6686e042535bc8ea642e16c72ff9e0cac81d87c8
vineetkrr <vineet.kumar@my.jcu.edu.au>
2025-11-09 19:26:43 +1000

    Start to implement main function

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -5,8 +5,32 @@ Estimate: 90 minutes
 Actual:
 """
 
+DEFAULT_FILENAME = "projects.txt"
+
+
 def main():
-    pass
+    """Main function to run the Project Management Program."""
+    print("Welcome to Pythonic Project Management")
+    projects = load_projects(DEFAULT_FILENAME)
+    print(f"Loaded {len(projects)} projects from {DEFAULT_FILENAME}")
 
+    choice = display_menu()
+    while choice != "Q":
+        if choice == "L":
+            pass
+        elif choice == "S":
+            pass
+        elif choice == "D":
+            pass
+        elif choice == "F":
+            pass
+        elif choice == "A":
+            pass
+        elif choice == "U":
+            pass
+        else:
+            print("Invalid choice")
 
-main()
+        choice = display_menu()
+
+    print("Thank you for using custom-built project management software.")

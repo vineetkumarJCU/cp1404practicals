134c3aa0fe139ba634c51fd0af69a54bc60610f0
vineetkrr <vineet.kumar@my.jcu.edu.au>
2025-11-22 22:03:13 +1000

    Implement drive method with random reliability check

--- prac_09/unreliable_car.py
+++ prac_09/unreliable_car.py
@@ -1,4 +1,5 @@
 from prac_09.car import Car
+from random import randint
 
 
 class UnreliableCar(Car):
@@ -6,3 +7,11 @@ class UnreliableCar(Car):
     def __init__(self, name, fuel, reliability):
         super().__init__(name, fuel)
         self.reliability = reliability
+
+    def drive(self, distance):
+        """Drive the car based on reliability."""
+        random_number = randint(0, 100)
+        if random_number < self.reliability:
+            return super().drive(distance)
+        else:
+            return 0

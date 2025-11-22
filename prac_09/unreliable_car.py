393843f7ea65393c9c0cb53eaecc66296b2b0a99
vineetkrr <vineet.kumar@my.jcu.edu.au>
2025-11-22 22:10:34 +1000

    Add docstrings to the file

--- prac_09/unreliable_car.py
+++ prac_09/unreliable_car.py
@@ -1,10 +1,16 @@
-from prac_09.car import Car
+"""
+CP1404/CP5632 Practical
+UnreliableCar class that inherits from Car
+"""
 from random import randint
+from prac_09.car import Car
 
 
 class UnreliableCar(Car):
+    """Specialised Car class that includes reliability for driving."""
 
     def __init__(self, name, fuel, reliability):
+        """Initialise an UnreliableCar instance."""
         super().__init__(name, fuel)
         self.reliability = reliability
 

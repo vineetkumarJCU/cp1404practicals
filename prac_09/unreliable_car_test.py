0823e85156ca96c60d04c51b2fbd49817591a0f3
vineetkrr <vineet.kumar@my.jcu.edu.au>
2025-11-23 00:22:45 +1000

    Improve test output format and add total distance counter

--- prac_09/unreliable_car_test.py
+++ prac_09/unreliable_car_test.py
@@ -1,13 +1,20 @@
+"""
+Test program for the UnreliableCar class
+"""
 from prac_09.unreliable_car import UnreliableCar
 
 
 def main():
-    unreliable_car = UnreliableCar("Old Bomb", 100, 50)
-    print(f"Created car: {unreliable_car}")
+    """Test UnreliableCar over multiple drive attempts."""
+    unreliable_car = UnreliableCar("Safari SUV", 100, 50)
+    total_distance = 0
 
     for i in range(10):
         distance = unreliable_car.drive(10)
-        print(f"Attempt {i + 1}: Drove {distance} km. {unreliable_car}")
+        total_distance += distance
+        print(f"Attempt {i + 1}: Drove {distance} km, total = {total_distance} km")
+
+    print(f"\nFinal Car status: {unreliable_car}")
 
 
 main()

d337302f3b8ce245526260a504c87e82a11589bf
vineetkrr <vineet.kumar@my.jcu.edu.au>
2025-11-23 13:42:58 +1000

    Add str method

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -12,3 +12,7 @@ class SilverServiceTaxi(Taxi):
 
     def get_fare(self):
         return super().get_fare() + self.flagfall
+
+    def __str__(self):
+        return f"{super().__str__()} plus flagfall of ${self.flagfall:.2f}"
+

928c8a6b2310fbefcf105e8958984cbfcce92549
vineetkrr <vineet.kumar@my.jcu.edu.au>
2025-11-23 14:47:45 +1000

    Add basic taxi simulator setup

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -5,7 +5,34 @@ Estimate: 30 minutes
 Actual:
 """
 
-def main():
-    pass
+from prac_09.taxi import Taxi
+from prac_09.silver_service_taxi import SilverServiceTaxi
 
-main()
+
+def main():
+    print("Let's drive!")
+    taxis = [
+        Taxi("Prius", 100),
+        SilverServiceTaxi("Limo", 100, 2),
+        SilverServiceTaxi("Hummer", 200, 4)
+    ]
+    bill_to_date = 0.0
+
+    menu = "q)uit, c)hoose taxi, d)rive"
+    print(menu)
+    choice = input(">>> ").lower()
+    while choice != "q":
+        if choice == "c":
+            pass
+        elif choice == "d":
+            pass
+        else:
+            print("Invalid option")
+        print(f"Bill to date: ${bill_to_date:.2f}")
+        print(menu)
+        choice = input(">>> ").lower()
+
+    print(f"Total trip cost: ${bill_to_date:.2f}")
+    print("Taxis are now:")
+
+

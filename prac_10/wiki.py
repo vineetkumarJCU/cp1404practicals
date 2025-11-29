b2b965e835064b8837e6ca0fdf49817c10c49855
vineetkrr <vineet.kumar@my.jcu.edu.au>
2025-11-29 23:47:58 +1000

    Implement Wikipedia page lookup and display basic page info

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -1,7 +1,13 @@
+import wikipedia
+
 def main():
     title = input("Enter page title: ").strip()
     while title != "":
-        print(f"Looking up: {title}")
+        page = wikipedia.page(title, autosuggest=False)
+        print(page.title)
+        print(page.summary)
+        print(page.url)
+        print()
         title = input("Enter page title: ").strip()
     print("Thank you.")
 
